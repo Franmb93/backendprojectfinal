b4c0a16ed794f8400de16998aeda4af9090c4bc0
Franmb93 <fcomunozbetanzos@gmail.com>
2022-01-18 10:25:31 +0100

    Estado inicial

---
+++ src/main/java/com/capgemini/BackendFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendFinalProjectApplication.class, args);
+	}
+
+}
