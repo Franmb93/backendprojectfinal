f98f03f4931363f6227bf35e84d4ff398ed92743
franmb93 <fcomunozbetanzos@gmail.com>
2022-02-13 19:57:47 +0100

    Added querys for product-category product-user

--- src/main/java/com/capgemini/services/IProductService.java
+++ src/main/java/com/capgemini/services/IProductService.java
@@ -10,5 +10,6 @@ public interface IProductService {
 	public Product findById(long id);
 	public Product save(Product product);
 	public void delete(long id);
-	
+	public List<Product> findByUserId(long id);
+	public List<Product> findByCategoryId(long id);
 }
